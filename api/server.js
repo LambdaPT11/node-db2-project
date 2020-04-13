af7f5617c50a01b2dedf1cb3a70bce08898ae132
Mason52171 <44660994+Mason52171@users.noreply.github.com>
2020-04-13 17:44:53 -0400

    server running and get at / and .api.cars/ hits server. api/cars sends created error

---
+++ api/server.js
@@ -0,0 +1,14 @@
+const express = require("express");
+
+const CarsRouter = require("../cars/cars-router");
+
+const server = express();
+
+server.use(express.json());
+server.use("/api/cars", CarsRouter);
+
+server.get('/', (req, res) => {
+    res.status(200).json({ api: "running" });
+});
+
+module.exports = server;
